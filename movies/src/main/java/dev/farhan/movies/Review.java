0cfdbb65900c53bc3c8d0aae25d34cb5479a17bf
Naman Singh Rana <namanr738@gmail.com>
2024-08-13 22:34:29 +0530

    chore: Add initial project files and configurations

---
+++ movies/src/main/java/dev/farhan/movies/Review.java
@@ -0,0 +1,27 @@
+package dev.farhan.movies;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    private ObjectId id;
+
+    public Review(String body) {
+        this.body = body;
+    }
+
+    private String body;
+
+}
